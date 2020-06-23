0da396060c96b4f903e81c89473610fd0ae87687
Colin <racezhao@163.com>
2020-06-23 09:32:01 +0800

    chore: add vue-router

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>Vite Router</div>
+  <div class="nav">
+    <router-link to="/home">Home</router-link>
+    <router-link to="/user/12">User</router-link>
+    <router-link to="/desc">Desc</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+  .nav {
+    padding: 10px;
+  }
+  .nav a {
+    margin: 10px;
+  }
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>user: {{ id }}</div>
+</template>
+
+<script>
+export default {
+  name: 'user',
+  props: {
+    id: String
+  },
+  setup (...args) {
+    console.log(args)
+  }
+}
+</script>
